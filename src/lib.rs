39ecb79923b12de06e2ab6dbe91ac03b52d119cc
yds12 <yds12@protonmail.com>
2022-09-03 13:40:51 +0200

    Grammar type works

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,77 @@
+struct Grammar(NonTerminal, Vec<Rule>);
+struct Rule(NonTerminal, RuleBody);
+enum RuleBody {
+    Or(Or),
+    Sequence(Sequence),
+}
+struct Or(Vec<Symbol>);
+struct Sequence(Vec<Symbol>);
+enum Symbol {
+    NonTerminal(NonTerminal),
+    Terminal(Terminal),
+}
+struct NonTerminal(String);
+struct Terminal(String);
+
+macro_rules! tsym {
+    ($string:expr) => {
+        Symbol::Terminal(Terminal($string.into()))
+    };
+}
+
+macro_rules! ntsym {
+    ($string:expr) => {
+        Symbol::NonTerminal(NonTerminal($string.into()))
+    };
+}
+
+macro_rules! nt {
+    ($string:expr) => {
+        NonTerminal($string.into())
+    };
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn can_build_grammar() {
+        let grammar = Grammar(
+            nt!("S"),
+            vec![
+                Rule(
+                    nt!("S"),
+                    RuleBody::Sequence(Sequence(vec![ntsym!("A"), ntsym!("N")])),
+                ),
+                Rule(
+                    nt!("A"),
+                    RuleBody::Or(Or(vec![
+                        tsym!("a"),
+                        tsym!("b"),
+                        tsym!("c"),
+                        tsym!("d"),
+                        tsym!("e"),
+                        tsym!("f"),
+                        tsym!("g"),
+                    ])),
+                ),
+                Rule(
+                    nt!("N"),
+                    RuleBody::Or(Or(vec![
+                        tsym!("0"),
+                        tsym!("1"),
+                        tsym!("2"),
+                        tsym!("3"),
+                        tsym!("4"),
+                        tsym!("5"),
+                        tsym!("6"),
+                        tsym!("7"),
+                        tsym!("8"),
+                        tsym!("9"),
+                    ])),
+                ),
+            ],
+        );
     }
 }
